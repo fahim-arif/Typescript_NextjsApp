081b1032096778e6fcd889b02f39b5ee862aa9c3
Raj Asha <rajcoder7@gmail.com>
2021-08-06 15:36:48 +0530

    added dummy design template for landing page

--- components/NewsletterBox.js
+++ components/NewsletterBox.js
@@ -4,6 +4,8 @@ import ClipLoader from "react-spinners/ClipLoader";
 import Message from "./Message";
 
 export default function NewsletterBox() {
+  const [fullname, setFullName] = useState("");
+  const [contact, setContact] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState({});
   const [loading, setLoading] = useState(false);
@@ -33,27 +35,46 @@ export default function NewsletterBox() {
   };
 
   return (
-    <div className="border p-8">
-      <h1 className="text-3xl font-bold text-gray-700 mb-4">
+    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+      <h1 className="text-xl font-medium text-gray-800 mb-4">
         Subscribe to the Newsletter
       </h1>
       <p className="text-gray-700 my-4">
         Get emails about updates and articles relating to the product.
       </p>
 
-      <form onSubmit={subscribe} className="flex items-center mb-4">
+      <form onSubmit={subscribe} className="flex flex-col space-y-4 mb-4">
+        <input
+          type="text"
+          name="fullname"
+          placeholder="Enter Full Name"
+          value={fullname}
+          onChange={(e) => setFullName(e.target.value)}
+          className="mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
+        />
+
+        <input
+          type="text"
+          name="contact"
+          placeholder="Enter Contact No."
+          value={contact}
+          onChange={(e) => setContact(e.target.value)}
+          className="mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
+        />
+
         <input
           type="email"
           name="email"
-          placeholder="Enter email"
+          placeholder="Enter Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-72 mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
+          className="mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
         />
+
         <button
           disabled={loading}
           type="submit"
-          className="flex justify-center items-center w-24 bg-red-400 text-white text-sm font-bold py-2 px-4 rounded-lg hover:bg-red-500"
+          className="self-start flex justify-center items-center bg-red-400 text-white text-base py-1 px-3 rounded hover:bg-red-500"
         >
           {!loading && <span>Subscribe</span>}
           {loading && <ClipLoader color="#fff" loading={true} size={20} />}
