18162a4edf7313e287a398e1d336ef660b7c629a
Raj Asha <rajcoder7@gmail.com>
2021-08-06 16:45:25 +0530

    added form validation for newsletter form using react-form-hook

--- components/Message.js
+++ components/Message.js
@@ -1,11 +1,12 @@
-export default function Message({ text, type, className }) {
+export default function Message({ message, type, className }) {
+  console.log("Props", type, message);
   return (
     <div className={className}>
       {type === "error" && (
-        <p className="text-red-400 text-sm font-bold">{text}</p>
+        <p className="text-red-400 text-sm font-bold">{message}</p>
       )}
       {type === "success" && (
-        <p className="text-green-400 text-sm font-bold">{text}</p>
+        <p className="text-green-400 text-sm font-bold">{message}</p>
       )}
     </div>
   );
--- components/NewsletterBox.js
+++ components/NewsletterBox.js
@@ -1,17 +1,34 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import ClipLoader from "react-spinners/ClipLoader";
 
 import Message from "./Message";
 
+let schema = yup.object().shape({
+  fullname: yup.string().required(),
+  contact: yup.number().required(),
+  email: yup.string().email().required(),
+});
+
 export default function NewsletterBox() {
-  const [fullname, setFullName] = useState("");
-  const [contact, setContact] = useState("");
-  const [email, setEmail] = useState("");
   const [message, setMessage] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const subscribe = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const subscribe = async (fields) => {
+    clearErrors();
 
     setLoading(true);
     setMessage({});
@@ -21,14 +38,19 @@ export default function NewsletterBox() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email }),
+      body: JSON.stringify({ email: fields.email }),
     });
 
     const data = await response.json();
-    setMessage(data);
 
     if (data.status === "success") {
-      setEmail("");
+      reset({}, { keepErrors: true });
+      setMessage(data);
+    } else {
+      setError("serverError", {
+        type: data.status,
+        message: data.text,
+      });
     }
 
     setLoading(false);
@@ -43,45 +65,64 @@ export default function NewsletterBox() {
         Get emails about updates and articles relating to the product.
       </p>
 
-      <form onSubmit={subscribe} className="flex flex-col space-y-4 mb-4">
+      <form onSubmit={handleSubmit(subscribe)} className="flex flex-col mb-4">
         <input
           type="text"
           name="fullname"
           placeholder="Enter Full Name"
-          value={fullname}
-          onChange={(e) => setFullName(e.target.value)}
-          className="mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
+          {...register("fullname")}
+          className="mt-2 mb-1 mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
         />
+        <p className="text-red-400 text-sm font-bold">
+          {errors.fullname?.message}
+        </p>
 
         <input
           type="text"
           name="contact"
           placeholder="Enter Contact No."
-          value={contact}
-          onChange={(e) => setContact(e.target.value)}
-          className="mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
+          {...register("contact")}
+          className="mt-2 mb-1 mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
         />
+        <p className="text-red-400 text-sm font-bold">
+          {errors.contact?.message}
+        </p>
 
         <input
           type="email"
           name="email"
           placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
+          {...register("email")}
+          className="mt-2 mb-1 mr-4 text-gray-700 border py-2 px-4 rounded-lg focus:outline-none"
         />
+        <p className="text-red-400 text-sm font-bold">
+          {errors.email?.message}
+        </p>
 
         <button
           disabled={loading}
           type="submit"
-          className="self-start flex justify-center items-center bg-red-400 text-white text-base py-1 px-3 rounded hover:bg-red-500"
+          className="w-24 mt-4 self-start flex justify-center items-center bg-red-400 text-white text-base py-1 px-3 rounded hover:bg-red-500"
         >
           {!loading && <span>Subscribe</span>}
           {loading && <ClipLoader color="#fff" loading={true} size={20} />}
         </button>
       </form>
 
-      <Message className="ml-1" type={message.status} text={message.text} />
+      {errors.serverError && (
+        <Message
+          className="ml-1"
+          type={errors.serverError?.type}
+          message={errors.serverError?.message}
+        />
+      )}
+      {message && (
+        <Message
+          className="ml-1"
+          type={message.status}
+          message={message.text}
+        />
+      )}
     </div>
   );
 }
