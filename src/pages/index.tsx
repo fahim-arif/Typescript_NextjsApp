53781e9fd7ac71fa30073e7630722e751a49e7cc
Raj Asha <rajcoder7@gmail.com>
2021-08-12 11:38:03 +0530

    replaced logo and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,24 @@ export default function Home() {
   return (
     <div className="app">
       <Head>
-        <title>twoMatches</title>
-        <meta name="description" content="twoMatches next app" />
+        <title>twomatches</title>
+        <meta name="description" content="twomatches next app" />
         <link rel="icon" href="/favicon.ico" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" 
+          rel="stylesheet" 
+        />
         <link
           href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
           rel="stylesheet"
         />
       </Head>
 
-      <main className="flex justify-center pt-8">
-        <div className="relative w-64 h-32">
+      <main className="flex flex-col justify-center items-center pt-8">
+        <div className="relative w-28 h-28">
           <Image src={logo} alt="twoMatches logo" layout="fill" />
         </div>
+        <p className="pt-4 text-4xl" style={{ fontFamily: "Montserrat, sans-serif" }}><span className="font-semibold">two</span>matches</p>
       </main>
 
       <Footer />
