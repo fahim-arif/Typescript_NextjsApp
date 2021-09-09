cdccc6f133795de03a2c9b9faf2ec57738f8bb30
Raj Asha <rajcoder7@gmail.com>
2021-09-09 12:14:08 +0530

    added yup validation for subscriber form fields

--- src/modules/Mailer/schema/Mailer.ts
+++ src/modules/Mailer/schema/Mailer.ts
@@ -1,10 +1,35 @@
 import * as yup from "yup";
 
 const MailerSchema = yup.object().shape({
-  first_name: yup.string().required(),
-  last_name: yup.string().required(),
-  email: yup.string().email().required(),
-  contact_no: yup.string().nullable(),
+  first_name: yup
+    .string()
+    .min(2)
+    .max(255)
+    .required()
+    .matches(/^[a-zA-Z .'-]+$/, "first name must be a valid name")
+    .label("first name"),
+  last_name: yup
+    .string()
+    .min(2)
+    .max(255)
+    .required()
+    .matches(/^[a-zA-Z .'-]+$/, "last name must be a valid name")
+    .label("last name"),
+  email: yup
+    .string()
+    .max(320)
+    .required()
+    .email()
+    .label("email"),
+  contact_no: yup
+    .string()
+    .min(10)
+    .max(15)
+    .matches(
+      /^\+[0-9]{1,14}$/,
+      "contact number must be of valid format +[country-code][number]"
+    )
+    .label("contact number"),
 });
 
 export default MailerSchema;
